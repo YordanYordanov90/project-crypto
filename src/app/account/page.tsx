ec486d88226c91587590449755ebee75981ed02d
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-09-02 11:03:53 +0300

    new

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,33 +1,30 @@
-import SavedCoin from '@/components/SavedCoin'
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import SavedCoin from "@/components/SavedCoin";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='max-w-[1140px] border-2 mt-20 shadow-xl rounded-lg w-full flex flex-col mx-auto'>
-        <div className='flex flex-col justify-between items-center my-12 py-4 rounded-div'>
+    <div className="max-w-[1140px] border-2 mt-20 shadow-xl rounded-lg w-full flex flex-col mx-auto">
+      <div className="flex flex-col justify-between items-center my-12 py-4 rounded-div">
+        <div>
+          <h1 className="text-2xl mb-5 font-bold">Account</h1>
           <div>
-            <h1 className='text-2xl mb-5 font-bold'>Account</h1>
-            <div>
-              <p>Welcome, {}</p>
-            </div>
-          </div>
-          <div>
-            <Button
-              
-              className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'
-            >
-              Sign Out
-            </Button>
+            <p>Welcome, {}</p>
           </div>
         </div>
-        <div className='flex justfiy-between items-center my-12 py-8 rounded-div'>
-          <div className='w-full '>
-            <h1 className='text-2xl font-bold py-4'>Watch List</h1>
-            <SavedCoin />
-          </div>
+        <div>
+          <Button className="border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl">
+            Sign Out
+          </Button>
         </div>
       </div>
-  )
-}
-export default page
+      <div className="flex justfiy-between items-center my-12 py-8 rounded-div">
+        <div className="w-full ">
+          <h1 className="text-2xl tex-center font-bold py-4">Watch List</h1>
+          <SavedCoin />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default page;
